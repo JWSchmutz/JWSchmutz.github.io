dd99a48bfe8027a7c7bab09e9b3704df40b3c626
Jeremiah Schmutz <Jeremiah.Schmutz@gmail.com>
2021-04-07 12:49:43 -0400

    switching to react portfolio

---
+++ src/data/data.js
@@ -0,0 +1,54 @@
+const data = [
+  {
+    groupName: "Handlebars",
+    projects: [
+      {
+        title: "Week Medical Supplies",
+        url: "https://weekmedicalsupplies.herokuapp.com/",
+        image: "week-medical-supplies.jpg",
+      },
+      {
+        title: "Seagrove TCG",
+        url: "https://www.seagrovetcg.com/",
+        image: "seagrove-tcg.jpg",
+      },
+      {
+        title: "Patient Link",
+        url: "https://patient-link.herokuapp.com/",
+        image: "patient-link.jpg",
+      },
+    ],
+  },
+  {
+    groupName: "React",
+    projects: [
+      {
+        title: "NBA Memory",
+        url: "https://jwschmutz.github.io/clicky/",
+        image: "clicky.jpg",
+      },
+      {
+        title: "This Portfolio",
+        url: "https://jwschmutz.github.io/",
+        image: "portfolio.jpg",
+      },
+    ],
+  },
+  {
+    groupName: "Games",
+    projects: [
+      {
+        title: "Beat The Shroom",
+        url: "https://jwschmutz.github.io/Fighting-Game/",
+        image: "beat-the-shroom.jpg",
+      },
+      {
+        title: "Pokemon RPG",
+        url: "https://jwschmutz.github.io/PokemonRPG/",
+        image: "pokemon-rpg.jpg",
+      },
+    ],
+  },
+];
+
+export default data;
